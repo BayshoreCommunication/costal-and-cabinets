ec47d274a0a672561016a3cf97c6544b10de447e
uiabu <ui.abukawsar@gmail.com>
2025-04-15 16:50:45 +0600

    Refactor code structure for improved readability and maintainability

--- components/home/HeroSection.jsx
+++ components/home/HeroSection.jsx
@@ -1,37 +1,105 @@
 "use client";
-import React, { memo, useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import React, { memo, useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import ScrollMotionEffect from "../motion/ScrollMotionEffect";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Autoplay,
+  Keyboard,
+  EffectFade,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+import { heroData } from "@/config/data";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const HeroSection = () => {
+  const prevButtonRef = useRef(null);
+  const nextButtonRef = useRef(null);
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (swiperRef.current && swiperRef.current.swiper) {
+        const swiperInstance = swiperRef.current.swiper;
+        swiperInstance.params.navigation.prevEl = prevButtonRef.current;
+        swiperInstance.params.navigation.nextEl = nextButtonRef.current;
+        swiperInstance.navigation.init();
+        swiperInstance.navigation.update();
+      }
+    }, 100); // small delay for DOM to be ready
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="relative w-full h-[500px] md:h-[700px] flex items-center justify-center">
-      {/* Background image */}
-      <Image
-        className="absolute inset-0 object-cover bg-bottom w-full h-full"
-        width={1920}
-        height={700}
-        src={"/assets/homepage/slides1.jpg"}
-        alt="Bg Image"
-      />
+    <>
+      <div className="flex items-center">
+        <button
+          ref={prevButtonRef}
+          className="hidden md:block text-white hover:bg-secondary duration-300 p-3 border border-white rounded-full z-40 absolute left-3 lg:left-9"
+        >
+          <FaArrowLeft size={20} />
+        </button>
 
-      {/* Gradient overlay */}
-      <div className="absolute inset-0 z-[1] bg-gradient-to-r from-black/50 to-black/50" />
+        <Swiper
+          ref={swiperRef}
+          effect={"fade"}
+          pagination={true}
+          keyboard={true}
+          loop={true}
+          autoplay={true}
+          autoplayDelay={2000}
+          autoplayDisableOnInteraction={false}
+          grabCursor={true}
+          speed={2000}
+          modules={[Navigation, Pagination, Keyboard, Autoplay, EffectFade]}
+          className="mySwiper"
+        >
+          {[...heroData].reverse().map((testimonial, index) => (
+            <SwiperSlide>
+              <div className="relative w-full h-[500px] md:h-[700px] flex items-center justify-center">
+                {/* Background image */}
 
-      {/* Centered text */}
-      <div className="container relative z-[2] flex flex-col items-center justify-center w-full">
-        <ScrollMotionEffect effect="fade-up" duration="2000">
-          <div className="flex items-center justify-center max-w-7xl w-full">
-            <h1 className="text-[40px] md:text-[60px] font-bold text-white text-center leading-snug md:leading-tight">
-              Your one stop shop for 5-star kitchen design, cabinets and
-              remodeling
-            </h1>
-          </div>
-        </ScrollMotionEffect>
+                <Image
+                  key={index}
+                  className="absolute inset-0 object-cover bg-bottom w-full h-full zoom-in-fade"
+                  width={1920}
+                  height={700}
+                  src={testimonial.backgroundImage}
+                  alt="Bg Image"
+                />
+
+                {/* Gradient overlay */}
+                <div className="absolute inset-0 z-[1] bg-gradient-to-r from-black/50 to-black/50" />
+
+                {/* Centered text */}
+                <div className="container relative z-[2] flex flex-col items-center justify-center w-full">
+                  <ScrollMotionEffect effect="fade-left" duration="1000">
+                    <div className="flex items-center justify-center max-w-7xl w-full">
+                      <h1 className="text-2xl md:text-[60px] font-bold text-white text-center leading-snug md:leading-tight">
+                        {testimonial.heading}
+                      </h1>
+                    </div>
+                  </ScrollMotionEffect>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <button
+          ref={nextButtonRef}
+          className="hidden md:block text-white hover:bg-secondary duration-300 p-3 border border-white rounded-full z-40 absolute right-3 lg:right-9"
+        >
+          <FaArrowRight size={20} />
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
