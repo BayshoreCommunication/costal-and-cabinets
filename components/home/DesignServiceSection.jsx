36eb4d0b8a566edeb010f9cd5888737b7553a6d9
uiabu <ui.abukawsar@gmail.com>
2025-04-16 11:45:30 +0600

    refactor: enhance Delivery, Design, and Sales sections with scroll motion effects; remove NewsSection

--- components/home/DesignServiceSection.jsx
+++ components/home/DesignServiceSection.jsx
@@ -21,61 +21,65 @@ const DesignServiceSection = () => {
         </ScrollMotionEffect>
         <div className="mt-10 md:mt-12">
           <div className="flex flex-col lg:flex-row gap-10 lg:gap-20 items-start justify-between">
-            <div className="w-full h-auto max-w-lg">
-              <h2 className="text-4xl lg:text-5xl font-bold mb-3 lg:mb-5 xl:mb-8">
-                Design Service
-              </h2>
-              <ul className="text-lg md:text-xl text-[#555555]  space-y-2 lg:space-y-5">
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  Kitchens, Vanities, Closets and more
-                </li>
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  On-Site Measuring
-                </li>
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  Showroom or Video Meeting
-                </li>
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  Selection Assistance
-                </li>
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  3D Renderings
-                </li>
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  Elevations
-                </li>
-                <li className="flex items-center gap-x-2">
-                  <GrRadialSelected />
-                  Samples and Showroom
-                </li>
-              </ul>
+            <ScrollMotionEffect effect="fade-right" duration="2000">
+              <div className="w-full h-auto max-w-lg">
+                <h2 className="text-4xl lg:text-5xl font-bold mb-3 lg:mb-5 xl:mb-8">
+                  Design Service
+                </h2>
+                <ul className="text-lg md:text-xl text-[#555555]  space-y-2 lg:space-y-5">
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    Kitchens, Vanities, Closets and more
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    On-Site Measuring
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    Showroom or Video Meeting
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    Selection Assistance
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    3D Renderings
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    Elevations
+                  </li>
+                  <li className="flex items-center gap-x-2">
+                    <GrRadialSelected />
+                    Samples and Showroom
+                  </li>
+                </ul>
 
-              <div className="group mt-8 inline-block">
-                <Link href={""}>
-                  <button className="relative overflow-hidden bg-primary text-white py-2 lg:py-3 lg:px-8 px-4 rounded-md">
-                    <span className="relative z-10 transition-colors duration-300">
-                      Call Costal & Cabinets
-                    </span>
-                    <span className="absolute inset-0 bg-secondary z-0 transform scale-x-0 origin-center group-hover:scale-x-100 transition-transform duration-500 ease-out"></span>
-                  </button>
-                </Link>
+                <div className="group mt-8 inline-block">
+                  <Link href={""}>
+                    <button className="relative overflow-hidden bg-primary text-white py-2 lg:py-3 lg:px-8 px-4 rounded-md">
+                      <span className="relative z-10 transition-colors duration-300">
+                        Call Costal & Cabinets
+                      </span>
+                      <span className="absolute inset-0 bg-secondary z-0 transform scale-x-0 origin-center group-hover:scale-x-100 transition-transform duration-500 ease-out"></span>
+                    </button>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div>
-              <Image
-                src="/assets/homepage/design-service.png"
-                alt="Design Service"
-                width={1000}
-                height={1000}
-                className="w-full h-auto max-w-lg"
-              ></Image>
-            </div>
+            </ScrollMotionEffect>
+            <ScrollMotionEffect effect="fade-left" duration="2000">
+              <div>
+                <Image
+                  src="/assets/homepage/design-service.png"
+                  alt="Design Service"
+                  width={1000}
+                  height={1000}
+                  className="w-full h-auto max-w-lg"
+                ></Image>
+              </div>
+            </ScrollMotionEffect>
           </div>
         </div>
       </div>
