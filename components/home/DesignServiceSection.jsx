5237b35b63208999b7217e27d2eb620c990718f5
uiabu <ui.abukawsar@gmail.com>
2025-04-28 18:32:25 +0600

    refactor: remove unused imports in DesignServiceSection component

--- components/home/DesignServiceSection.jsx
+++ components/home/DesignServiceSection.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import SectionLayout from "../shared/SectionLayout";
 import Link from "next/link";
-import ScrollMotionEffect from "../motion/ScrollMotionEffect";
 import { GrRadialSelected } from "react-icons/gr";
 import Image from "next/image";
 
